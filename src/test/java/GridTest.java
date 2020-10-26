8b9f8f9d40298824dbec90e82f41236cbed13c3b
unknown <l.selven97@gmail.com>
2020-10-26 10:50:52 +0100

    change import

--- src/test/java/GridTest.java
+++ src/test/java/GridTest.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 
 public class GridTest {
