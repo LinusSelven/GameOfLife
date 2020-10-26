5faf4bc74f372cbf9014173f56169ab84bc944b6
unknown <l.selven97@gmail.com>
2020-10-26 10:49:50 +0100

    add test for checking that grid only contains two different characters. add before each setup

--- src/test/java/GridTest.java
+++ src/test/java/GridTest.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -10,6 +11,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class GridTest {
 
     Grid grid;
+
+    @BeforeEach
+    void setup() {
+        int[][] tempGrid = new int[][]{
+                {1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+        };
+        grid = new Grid(tempGrid);
+    }
 
     @Test
     void checkThatGridCreatedHasTheSameValuesAsTheOneWeSentIn() {
@@ -28,6 +46,12 @@ public class GridTest {
         grid = new Grid(tempGrid);
 
         assertThat(Arrays.deepEquals(grid.grid, tempGrid)).isTrue();
+    }
+
+    @Test
+    void checkGridForTwoDifferentCharacters(){
+        IntStream stream = Arrays.stream(grid.grid).flatMapToInt(Arrays::stream);
+        assertEquals(2, stream.distinct().count());
     }
 }
 
