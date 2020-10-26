525ce7b44787860098328da989160b3b7d5b6245
unknown <l.selven97@gmail.com>
2020-10-26 11:44:12 +0100

    add compare method

--- src/main/java/Grid.java
+++ src/main/java/Grid.java
@@ -1,8 +1,16 @@
+import java.util.Arrays;
+
 public class Grid {
 
     public final int[][] grid;
 
     public Grid(int[][] grid) {
         this.grid = grid;
+    }
+
+
+    public boolean compareGrids(Grid futureGrid) {
+        return Arrays.deepEquals(grid, futureGrid.grid);
+
     }
 }
