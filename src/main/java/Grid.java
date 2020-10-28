d3117fe43a9a5ff19fbc5d799cf897a1a86b7191
Skoorps <skoorps@gmail.com>
2020-10-28 20:40:48 +0100

    add default constructor

--- src/main/java/Grid.java
+++ src/main/java/Grid.java
@@ -12,6 +12,21 @@ class Grid {
         this.grid = new int[size][size];
     }
 
+    Grid() {
+        this.grid = new int[][]{
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 1, 1, 1, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 1, 1, 1, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+        };
+    }
+
 
     boolean compareGrids(Grid gridToCompareTo) {
         return Arrays.deepEquals(grid, gridToCompareTo.grid);
