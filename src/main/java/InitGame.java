7498146c754f737f5341785c6c41bce358915a4f
Skoorps <skoorps@gmail.com>
2020-10-28 20:42:04 +0100

    extract code from main

--- src/main/java/InitGame.java
+++ src/main/java/InitGame.java
@@ -1,35 +1,28 @@
 public class InitGame {
 
-    private static GameOfLife gol;
+
 
     public static void main(String[] args) {
         InitGame initGame = new InitGame();
-        gol = new GameOfLife();
-        Grid future = new Grid(5);
-        Grid grid = initGame.createGrid();
-        PrintGrid.printGameBoard(grid);
+        initGame.start(new GameOfLife(), initGame.createGrid());
+    }
 
-        while(initGame.gridsAreDifferent(grid, future)){
-            future = gol.nextGen(grid);
+    private Grid createGrid() {
+        return new Grid();
+    }
+
+    private void start(GameOfLife gameOfLife, Grid grid){
+        Grid future = new Grid(5);
+        PrintGrid.printGameBoard(grid);
+        while(gridsAreDifferent(grid, future)){
+            future = gameOfLife.nextGen(grid);
             PrintGrid.printGameBoard(future);
-            if (initGame.gridsAreDifferent(grid, future)) {
+            if (gridsAreDifferent(grid, future)) {
                 grid = future;
                 future = new Grid(5);
             }
         }
     }
-
-    private Grid createGrid() {
-        int[][] temp = new int[][]{
-                {0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0},
-                {0, 0, 1, 1, 0},
-                {0, 0, 1, 1, 0},
-                {0, 0, 0, 0, 0}
-        };
-        return new Grid(temp);
-    }
-
 
     private boolean gridsAreDifferent(Grid grid, Grid futureGrid) {
         return !grid.compareGrids(futureGrid);
